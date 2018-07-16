b686cc84d6c9223e17fd60ff6d2239185b17f0f0
Stefan Haun <tux@netz39.de>
2018-07-16 19:27:37 +0200

    Add form for downtime notification

--- ffmd/gluon-config-mode-notify-2016.2/check_site.lua
+++ ffmd/gluon-config-mode-notify-2016.2/check_site.lua
@@ -1,3 +1,3 @@
-if need_table('config_mode', nil, false) and need_table('config_mode.owner', nil, false) then
+if need_table('config_mode', nil, false) and need_table('owner', nil, false) then
   need_boolean('config_mode.owner.obligatory', false)
 end
